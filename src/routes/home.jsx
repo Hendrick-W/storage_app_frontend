c269276df3d61442d1272e7514def6378efc8119
Hendrick <hendrickwijaya21@gmail.com>
2021-03-30 16:49:56 +0700

    Set router, Header and home

---
+++ src/routes/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Homes from "../Component/home";
+import Header from "../Component/Header";
+
+
+function Home() {
+  return (
+    <div >
+      <Header />
+      <Homes />
+
+    </div>
+  );
+}
+
+export default Home;
