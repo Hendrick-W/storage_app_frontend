5a777d4721a94e0e188c1760f097514a9855a547
Hendrick <hendrickwijaya21@gmail.com>
2021-03-31 15:28:06 +0700

    Set the route

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,26 +1,26 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
 } from "react-router-dom";
 import Home from "../pages/home";
 import Profile from "../pages/Profile"
 
 export const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/Profile" exact>
-                    <Profile />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+      <Router>
+          <Switch>
+              <Route path="/" exact>
+                  <Home />
+              </Route>
+              <Route path="/Profile" exact>
+                  <Profile />
+              </Route>
+          </Switch>
+      </Router>
+  );
 };
 
 export default Routes;
